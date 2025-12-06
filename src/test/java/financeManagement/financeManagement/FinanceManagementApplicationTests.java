23a88bcb48532fdaf085230b85b685155dae8934
Sujay Awale <sujayawale125@gmail.com>
2025-12-06 14:49:03 +0530

    Pushing all code without any api

---
+++ src/test/java/financeManagement/financeManagement/FinanceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package financeManagement.financeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
